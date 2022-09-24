6111f0be5bfa58c4467491b1e446534d35140062
stavros <stavsamaras@gmail.com>
2022-09-24 13:29:24 +0300

    first commit

---
+++ src/main/java/com/stavsam/employeemanagement/EmployeeMainApplication.java
@@ -0,0 +1,13 @@
+package com.stavsam.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMainApplication.class, args);
+	}
+
+}
