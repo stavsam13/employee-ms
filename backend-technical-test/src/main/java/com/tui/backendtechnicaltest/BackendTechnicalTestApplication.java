19180e7c3baddcd17256f44ff26d04aadd4f043c
stavsam13 <stavsamaras@gmail.com>
2022-09-24 13:03:06 +0300

    Add files via upload

---
+++ backend-technical-test/src/main/java/com/tui/backendtechnicaltest/BackendTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.tui.backendtechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ backend-technical-test/src/test/java/com/tui/backendtechnicaltest/BackendTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tui.backendtechnicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
